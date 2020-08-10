bc60987b6c599191f2c06dcc57d46a952e294bfc
Philip Sailer <philsailer@gmail.com>
2020-08-10 20:49:46 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,10 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-  index>=0 && index<=8 && not(position_taken?(board,index))
+  if index>=0 && index<=8
+    not(position_taken?(board,index))
+  else
+    false
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
